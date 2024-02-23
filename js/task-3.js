b3d09f6093cf193a81abcdf88e0edb484752f51c
Zhanna14 <tkachukzhanna25@gmail.com>
2024-02-23 14:43:28 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-const nameInput = document.getElementById('name-input');
-const nameOutput = document.getElementById('name-output');
+const nameInput = document.querySelector('name-input');
+// const nameOutput = document.getElementById('name-output');
 
 nameInput.addEventListener('input', event => {
   const trimmedValue = event.currentTarget.value.trim();
