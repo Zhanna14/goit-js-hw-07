d4c2fed35ae8b72450e1177559e4a80a2edde96c
Zhanna14 <tkachukzhanna25@gmail.com>
2024-02-24 09:08:09 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,11 @@
-const nameInput = document.querySelector('name-input');
-// const nameOutput = document.getElementById('name-output');
-
-nameInput.addEventListener('input', event => {
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.getElementById('name-output');
+nameInput.addEventListener('input', handleInput);
+function handleInput(event) {
   const trimmedValue = event.currentTarget.value.trim();
-
   if (trimmedValue !== '') {
     nameOutput.textContent = trimmedValue;
   } else {
     nameOutput.textContent = 'Anonymous';
   }
-});
+}
