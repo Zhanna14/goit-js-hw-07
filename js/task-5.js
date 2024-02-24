7b45ca19b3363f32c8064efc05afa87ef85f1698
Zhanna14 <tkachukzhanna25@gmail.com>
2024-02-24 10:34:48 +0100

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+const clickMe = document.querySelector('.change-color');
+const color = document.querySelector('body');
+const valueColor = document.querySelector('.color')
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+clickMe.addEventListener('click', handleClick);
+
+
+function handleClick() {
+  color.style.backgroundColor = getRandomHexColor();
+  valueColor.textContent = getRandomHexColor();
+}
+
+
