9983628b21710af7e5866667a2d9bb4f34959049
Zhanna14 <tkachukzhanna25@gmail.com>
2024-02-25 12:49:56 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,4 +1,4 @@
-const number = document.querySelector('input');
+const inputAmount = document.querySelector('input');
 const box = document.querySelector('#boxes');
 
 function getRandomHexColor() {
@@ -7,38 +7,26 @@ function getRandomHexColor() {
     .padStart(6, '0')}`;
 }
 
-number.addEventListener('input', handleInput); // Призначаємо обробник подій для події зміни значення поля вводу
-
-function handleInput(event) {
-  const value = parseInt(number.value);
-  console.log(value);
-
-  if (value < 1 || value > 100) {
-    number.value = '';
-  }
-}
-
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 
 createButton.addEventListener('click', () => {
-  box.innerHTML = '';
+  box.innerHTML = "";
   let size = 30;
-  const value = parseInt(number.value);
+  const value = parseInt(inputAmount.value);
 
   for (let i = 1; i <= value; i += 1) {
     const square = document.createElement('div');
     square.style.width = `${size}px`;
     square.style.height = `${size}px`;
     square.style.backgroundColor = getRandomHexColor();
-    square.addEventListener('click', () => box.removeChild(square));
     box.appendChild(square);
     size += 10;
   }
 
-  number.value = '';
+  inputAmount.value = "";
 });
 
 destroyButton.addEventListener('click', () => {
-  box.innerHTML = '';
+  box.innerHTML = "";
 });
