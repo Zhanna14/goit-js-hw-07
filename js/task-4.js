bf08b375850668c5a9aab51feb86384006e734b5
Zhanna14 <tkachukzhanna25@gmail.com>
2024-02-25 12:34:49 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,13 +1,14 @@
 const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', handleSubmit);
+
 function handleSubmit(event) {
   event.preventDefault();
   const elements = event.currentTarget.elements;
-
   const userEmail = elements.email.value.trim();
   const userPasword = elements.password.value.trim();
-  if (userEmail === '' || userPasword === '') {
+  
+  if (userEmail === "" || userPasword === "") {
     alert(`All form fields must be filled in`);
   } else {
     const info = {
