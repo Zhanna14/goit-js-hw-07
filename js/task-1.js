5daa8557adebab499618101dfb2d5e9f4f2113ab
Zhanna14 <tkachukzhanna25@gmail.com>
2024-02-25 11:27:13 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -4,6 +4,6 @@ console.log('Number of categories: ', categoryItems.length);
 categoryItems.forEach(function (categoryItem) {
   const text = categoryItem.querySelector('h2');
   const lengthItem = categoryItem.querySelectorAll('li');
-  console.log(`Category: `, text.textContent);
-  console.log(`Elements: `, lengthItem.length);
+  console.log(`Category: ${text.textContent}`);
+  console.log(`Elements: ${lengthItem.length}`);
 });
